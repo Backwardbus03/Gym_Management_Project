d68b74ec2834fef5dd4220a570c0190d69ab5e9b
Backwardbus03 <backwardbus03@gmail.com>
2025-04-21 18:43:05 +0530

    added requirements.txt

--- gymmanagement/gymmanagement/settings.py
+++ gymmanagement/gymmanagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "gymmanagement" / "static",
 ]
 
+ALLOWED_HOSTS = ['gymmanagement.onrender.com']
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
